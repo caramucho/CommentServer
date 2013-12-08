defb1d472f5155245f9e4536ddb8a9c585c746a8
rockyou21 <rockyouruby@gmail.com>
2013-12-08 17:17:05 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var model = require('../model');
+var Post = model.Post;
+var items = [
+    { "text": "1st Post." }
+      , { "text": "2nd Post." }
+      ];
+
+exports.index = function(req, res){
+    Post.find({}, function(err, items){
+            res.render('index', { title: 'Entry List', items: items })
+          });
+};
+
+exports.form = function(req, res){
+    res.render('form', { title: 'New Entry' })
+};
+
+exports.create = function(req, res){
+    var newPost = new Post(req.body);
+      newPost.save(function(err){
+             if (err) {
+                      console.log(err);
+                             res.redirect('back');
+                                  } else {
+                                           res.redirect('/');
+                                                }
+                 });
+};
